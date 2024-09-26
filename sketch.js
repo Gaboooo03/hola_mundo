89bb1c0c355aa82a87932594c9a13bda71efe43a
gabriel <gabrielrsebastianrios@gmail.com>
2024-09-26 15:44:36 -0500

    se creo un cursor bien bonito c:

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+createCanvas (windowWidth, windowHeight);
+frameRate(300);
+background (0);
+rectMode(CENTER);
+
+}
+function draw(){
+background(random(90, 100), random(30, 130),0);
+fill(255, mouseX, 54);
+circle(mouseX + 70, mouseY, mouseX );
+fill(150, 200, 3);
+rect(mouseX, mouseY,100,100);
+
+
+}
