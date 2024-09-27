3d7cdc50bb2f541686941dc4329f6dba8a7c552b
gabriel <gabrielrsebastianrios@gmail.com>
2024-09-27 12:58:06 -0500

    Se crea mouse con diferentes circulos y cuadrados con colores

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
 function setup(){
 createCanvas (windowWidth, windowHeight);
-frameRate(300);
+frameRate(60);
 background (0);
 rectMode(CENTER);
+noCursor();
+
 
 }
 function draw(){
-    background(90, random(30, 80),90 ,10);
+    background(255, 40,60, 2);
+    cursor(CROSS);
+    circle(mouseX,0, 80);
+    noFill();
+    circle(mouseX,725, 80);
+    noFill();
+    circle(1525,mouseY, 50);
+    noFill();
+    circle(0,mouseY, 50);
+    noFill();
 
-circle(mouseX,mouseY, 50);
-noStroke();
-fill(random(10, 255), random(20,255), random(32,56,211));
-
-
+    
+    circle(mouseX,mouseY, 100);
+    
+    circle(mouseX,mouseY, 80);
+    circle(mouseX,mouseY, 60);
+    circle(mouseX,mouseY, 50);
+    circle(mouseX,mouseY, 40);
+    circle(mouseX,mouseY, 30);
+    rect(mouseX,mouseY,30 ,30);
+    
+    strokeWeight(10);
+    stroke(mouseX,mouseY,random(0,255));
+strokeWeight(1);
 
 
 
